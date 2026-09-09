1f3af98a594595a529b48b4255c1a5972518ce0f
Ayushman <ayushmanpanda120@gmail.com>
2026-09-09 20:22:06 +0530

    new chapter

---
+++ ConsoleAndInput/problem7.1.py
@@ -0,0 +1,14 @@
+
+#?Problem 7.1
+
+#!
+
+radius = float(input("Enter the radius of the circle: "))
+length = int(input("Enter the length of the rectangle: "))
+breadth = int(input("Enter the breadth of the rectangle: "))
+
+c = 3.14*2*radius
+p = 2*(length+breadth)
+
+print("circumference of circle is ", c)
+print("perimeter of rectangle is ", p)
