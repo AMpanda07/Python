c34029be08544e01aebb0e2455cf715f54258364
Ayushman <ayushmanpanda120@gmail.com>
2026-09-19 10:23:50 +0530

    functions

---
+++ Basics/perfect.py
@@ -0,0 +1,12 @@
+
+def perfect(n):
+    sum = 0
+    for i in range(1,n):
+        if n % i == 0:
+            sum = sum + i
+    return sum
+n =int(input("Enter a number: "))
+if n == perfect(n):
+    print("perfect number")
+else:
+    print("Not Perfect")
---
+++ Basics/q21.py
@@ -0,0 +1,10 @@
+
+#? findout even odd using function
+
+def EvenOdd():
+    n = int(input("Enter your number: "))
+    if n%2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+EvenOdd()
