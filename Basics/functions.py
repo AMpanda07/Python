8740866cf0d9084a66d41ece75ede9f6a953d1dd
Ayushman <ayushmanpanda120@gmail.com>
2026-09-19 09:11:41 +0530

    functions

---
+++ Basics/functions.py
@@ -0,0 +1,13 @@
+
+#* Functions and recursion
+n=int(input("enter count down: "))
+
+
+
+def bomb(n):
+    if not n:
+        print("A")
+    else:
+        for n in range(n,0,-1):
+            print(n)
+bomb(n)
